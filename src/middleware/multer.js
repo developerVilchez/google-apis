a5dd3a61dbfe2e5669b0700b52338606d204b2a3
soporteclientesanonimos <developervilchez@gmail.com>
2020-10-22 02:45:45 -0500

    definiendo el destino de los files

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,10 +1,9 @@
 const multer = require('multer');
+
 const path = require('path');
 
-//definimos como quiero que guarde la data multer
-
 const storage = multer.diskStorage({
-  destination :  './public/uploads/',
+  destination :  path.join(path.dirname(__dirname), 'public/uploads'),
   filename : (req, file, cb) => {
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
   }
