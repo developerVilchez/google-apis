e0465383c3bf56ff2914a566a9859eed4db68234
soporteclientesanonimos <developervilchez@gmail.com>
2020-10-22 01:05:35 -0500

    agrega carpeta rutas y vistas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-const homeRouter = require('../routes/home');
-const port = 5003 || process.env.PORT;
+//settings
+app.set('view engine', 'ejs');
+app.set('port', 5003);
+app.set('views', path.join(__dirname, 'views'))
+
+//muestra el path del archivo app.js
+//console.log(__dirname) 
+console.log(path.join(__dirname, 'views'))
+
+const homeRouter = require('./routes/home');
+//const port = 5003 || process.env.PORT;
 
 
+//traemos las rutas
 app.use('/', homeRouter);
+
+const port = app.get('port');
+console.log(port)
 
 
 app.listen(port, () => {
