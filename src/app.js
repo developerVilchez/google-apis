e58d67f32b546b4762c92e9904dbbb7626ebf1a1
soporteclientesanonimos <developervilchez@gmail.com>
2020-10-22 02:24:47 -0500

    limpia el codigo elimina carpeta de imagenes

--- src/app.js
+++ src/app.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+//Rutas
+const homeRouter = require('./routes/home');
+const uploadRouter = require('./routes/upload');
+
 
 //settings
 app.set('view engine', 'ejs');
 app.set('port', 5003);
 app.set('views', path.join(__dirname, 'views'))
 
-//Rutas
-const homeRouter = require('./routes/home');
-const uploadRouter = require('./routes/upload');
-
-
-
 //traemos las rutas
 app.use('/', homeRouter);
 app.use('/upload',uploadRouter);
 
 const port = app.get('port');
-console.log(port)
-
 
 app.listen(port, () => {
  console.log(`Escuchando peticiones desde el puerto ${port}`)
